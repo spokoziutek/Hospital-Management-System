1fdf44feb1500f10092d78138284e8b368ce37b1
Łukasz Bieskiewicz <122742419+spokoziutek@users.noreply.github.com>
2024-06-12 14:53:54 +0200

    Update func.php

--- func.php
+++ func.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","myhmsdb");
+require_once("include/config.php");
 if(isset($_POST['patsub'])){
 	$email=$_POST['email'];
 	$password=$_POST['password2'];
