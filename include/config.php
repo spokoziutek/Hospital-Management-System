5d1190c795ccacf1265bb46d44052702fe437da3
Łukasz Bieskiewicz <122742419+spokoziutek@users.noreply.github.com>
2024-06-12 09:22:26 +0200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hms');
+define('DB_SERVER','hospitalzaliczenie-server.mysql.database.azure.com');
+define('DB_USER','hjdoyovazf');
+define('DB_PASS' ,'qweasdzxc1!');
+define('DB_NAME', 'hospitalzaliczenie-database');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
