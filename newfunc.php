c9bf283b44364f37dc9ab82ea4fbdcc2ff23fbe9
Łukasz Bieskiewicz <122742419+spokoziutek@users.noreply.github.com>
2024-06-12 13:42:13 +0200

    Update newfunc.php

--- newfunc.php
+++ newfunc.php
@@ -1,6 +1,6 @@
 <?php
 // session_start();
-$con=mysqli_connect("localhost","root","","myhmsdb");
+require_once("include/config.php");
 // if(isset($_POST['submit'])){
 //  $username=$_POST['username'];
 //  $password=$_POST['password'];
